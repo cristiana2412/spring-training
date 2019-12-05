c2159d754c83f5e1730fc8b67ab00e8d0073ed08
student <cristiana.dmtr@gmail.com>
2019-12-05 16:15:45 +0200

    Added tests

--- team/src/main/java/com/example/team/repository/PersonRepository.java
+++ team/src/main/java/com/example/team/repository/PersonRepository.java
@@ -2,9 +2,15 @@ package com.example.team.repository;
 
 import com.example.team.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     List<Person> findByTeamId(Long id);
+
+    @Query(value = "update Person p set p.team = null where p.team.id = ?1", nativeQuery = true)
+    @Modifying
+    public void removePersonsFromTeam(Long teamId);
 }
--- team/src/test/java/com/example/team/TeamApplicationTests.java
+++ team/src/test/java/com/example/team/TeamApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.team;
 
+import com.example.team.repository.PersonRepository;
+import com.example.team.repository.TeamRepository;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TeamApplicationTests {
+	@Autowired
+	TeamRepository teamRepository;
+	@Autowired
+	PersonRepository personRepository;
 
 	@Test
 	void contextLoads() {
+
+
+	}
+
+	@Test
+	void shallFindTeams(){
+		assertTrue( teamRepository.findAll().size() > 0);
 	}
 
 }
